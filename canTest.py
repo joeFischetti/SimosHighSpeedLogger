e1fda2dcca89148eec1da3e0bdb85da1466a4649
Joe Fischetti <jkfischetti@gmail.com>
2021-02-01 01:38:51 +0000

    cantest stuff

--- canTest.py
+++ canTest.py
@@ -51,5 +51,5 @@ if args.testmode == "udsoncan":
         send_raw(b'2210c0')
 elif args.testmode == "python-can":
     for i in range(0,30):
-        send_raw_2(b'032210c0')
+        send_raw_2(b'2210c0')
 
