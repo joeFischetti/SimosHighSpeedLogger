fbb03a7eccc3c9789eda6551a264a89f377ceb4b
Joe Fischetti <jkfischetti@gmail.com>
2021-02-01 02:26:41 +0000

    pyLog mode22 work

--- pyLog.py
+++ pyLog.py
@@ -404,7 +404,7 @@ def getParams22():
             results = "62" + logParams[param]['location'].lstrip("0x") + str(hex(fakeVal)).lstrip('0x')
         else:
             results = (send_raw_2(bytes.fromhex('0322' + logParams[parameter]['location'].lstrip("0x") + "00000000"))).hex().rstrip('a')
-            print(str(results))
+            #print(str(results))
 
         if results.startswith("0562"):
         
