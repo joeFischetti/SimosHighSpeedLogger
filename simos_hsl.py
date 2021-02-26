b68c96920e60ddf68dc678ea31762e7dfe3d2861
Joe Fischetti <jkfischetti@gmail.com>
2021-02-25 20:47:27 -0500

    updated call for get_ip

--- simos_hsl.py
+++ simos_hsl.py
@@ -134,7 +134,7 @@ class hsl_logger():
                     self.configuration = yaml.load(configFile)
             
                 if 'notification' in self.configuration:
-                    notificationEmail(configuration['notification'], "Starting logger with IP address: " + get_ip())
+                    notificationEmail(configuration['notification'], "Starting logger with IP address: " + self.get_ip())
     
             except Exception as e:
                 self.activityLogger.info("No configuration file loaded: " + str(e))
@@ -415,6 +415,19 @@ class hsl_logger():
 
         return theKey.to_bytes(4, 'big')
 
+    #Helper function that just gets the local IP address of the Pi (so we can email it as a notification for debugging purposes)
+    def get_ip():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
+
 
 
 
@@ -595,19 +608,6 @@ def getParams23():
 
         logFile.write(row + '\n')
 
-
-#Helper function that just gets the local IP address of the Pi (so we can email it as a notification for debugging purposes)
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
 
 
 
