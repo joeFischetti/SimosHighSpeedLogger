bf22952dc549132dc944a5bfcd6dbc2dc8bff8f7
Joe Fischetti <jkfischetti@gmail.com>
2021-03-02 21:21:08 -0500

    set default interface so it's OS aware

--- wrapper.py
+++ wrapper.py
@@ -2,6 +2,11 @@
 
 import argparse, simos_hsl
 
+if sys.platform == "win32":
+    defaultInterface = "J2534"
+else:
+    defaultInterface = "CAN"
+
 #build the argument parser and set up the arguments
 parser = argparse.ArgumentParser(description='Simos18 High Speed Logger')
 parser.add_argument('--headless', action='store_true')
@@ -11,7 +16,7 @@ parser.add_argument('--testing', help="testing mode, for use when not connected 
 parser.add_argument('--runserver', help="run an app server, used with the android app", action='store_true')
 parser.add_argument('--interactive', help="run in interactive mode, start/stop logging with the enter key", action='store_true')
 parser.add_argument('--mode', help="set the connection mode: 2C, 23")
-parser.add_argument('--interface', help="Optionally use a J2534 interface if running on windows", choices = ["J2534", "CAN", "TEST"], default="J2534" )
+parser.add_argument('--interface', help="Optionally use a J2534 interface if running on windows", choices = ["J2534", "CAN", "TEST"], default=defaultInterface )
 parser.add_argument('--singlecsv', help="Set the logger so that it writes all csv data to the same file instead of new files, separated with a row of 0's", action="store_true")
 
 
